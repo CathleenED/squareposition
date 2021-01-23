c3a257d617753c17a74a2af2473db4c0e3b2c75d
CathleenED <77340398+CathleenED@users.noreply.github.com>
2021-01-23 20:26:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var frect,mrect
+
+
+function setup() {
+
+
+  createCanvas(800,400);
+  frect=createSprite(400, 200, 50, 50);
+  frect.shapeColor="green"
+
+  mrect=createSprite(200,400,50,50)
+  mrect.shapeColor="green"
+  
+}
+
+function draw() {
+  background(255,255,255);  
+console.log(mrect.x-frect.x)
+  mrect.x=World.mouseX
+  mrect.y=World.mouseY
+
+
+  if (mrect.x-frect.x<mrect.width/2+frect.width/2 && frect.x-mrect.x<mrect.width/2+frect.width/2 &&
+    mrect.y-frect.y<mrect.height/2+frect.height/2 && frect.y-mrect.y < mrect.height/2+frect.height/2) {
+    mrect.shapeColor="blue"
+    frect.shapeColor="orange"
+  }
+  else {
+    mrect.shapeColor="green"
+    frect.shapeColor="green"
+  }
+  
+   
+
+
+  
+  drawSprites();
+}
